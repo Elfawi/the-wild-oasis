f681a01a057245649638f798b8a2df696de30f2e
Mohamed Elfawi <elfawi123@gmail.com>
2025-09-27 03:17:44 +0300

    more responsive form

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -51,10 +51,11 @@ const Button = styled.button`
   & svg {
     width: 2.4rem;
     height: 2.4rem;
-    /* Sometimes we need both */
-    /* fill: var(--color-grey-500);
-    stroke: var(--color-grey-500); */
     color: var(--color-grey-500);
+    @media (max-width: 800px) {
+      width: 3.4rem;
+      height: 3.4rem;
+    }
   }
 `;
 
